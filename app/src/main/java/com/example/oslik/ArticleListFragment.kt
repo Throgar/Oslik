141e16d1756f008b0afaa7b09c5d36e203c5b19f
Jakub Lehotsky <jakub.lehotsky@siemens-healthineers.com>
2020-03-03 17:38:07 +0100

    Working navigation

--- app/src/main/java/com/example/oslik/ArticleListFragment.kt
+++ app/src/main/java/com/example/oslik/ArticleListFragment.kt
@@ -1,6 +1,21 @@
 package com.example.oslik
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.oslik.databinding.FragmentArticleListBinding
 
 class ArticleListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentArticleListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
 }
--- app/src/main/java/com/example/oslik/OslikActivity.kt
+++ app/src/main/java/com/example/oslik/OslikActivity.kt
@@ -1,7 +1,15 @@
 package com.example.oslik
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil.setContentView
+import com.example.oslik.databinding.ActivityOslikBinding
 
 class OslikActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView<ActivityOslikBinding>(this, R.layout.activity_oslik)
+    }
 }
